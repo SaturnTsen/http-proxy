34f37a5fa208ffecbf7bd52fa38d863018830721
SaturnTsen <minger233@outlook.com>
2025-02-27 11:38:29 +0100

    amend

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: "http-proxy",
-      // patch the npm path
-      script: "C:\\nvm4w\\nodejs\\node_modules\\npm\\bin\\npm-cli.js",
-      watch: true,                  // 自动重启
-      autorestart: true,            // 应用崩溃时自动重启
-      args: "serve",
-      max_memory_restart: "500M",   // 内存占用超过 500MB 时重启
-      env: {
-        NODE_ENV: "production",     // 环境变量
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "http-proxy",
+            // patch the npm path
+            script: "C:\\nvm4w\\nodejs\\node_modules\\npm\\bin\\npm-cli.js",
+            watch: true,                  // 自动重启
+            autorestart: true,            // 应用崩溃时自动重启
+            args: "serve",
+            max_memory_restart: "500M",   // 内存占用超过 500MB 时重启
+            env: {
+                NODE_ENV: "production",     // 环境变量
+            },
+        },
+    ],
 };
